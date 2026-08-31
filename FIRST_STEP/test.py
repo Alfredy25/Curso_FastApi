0e15b14615f5fce2c6ff5940f6b35e048bfc3317
Alfredy25 <kiofriend17@gmail.com>
2026-08-31 17:54:56 -0600

    Se agregan archivos de primeros pasos usando el framework de fastapi

---
+++ FIRST_STEP/test.py
@@ -0,0 +1,23 @@
+BLOG_POST = [
+    {"id": 1, "title": "Hola desde FastAPI", "content": "Mi primer post con FastAPI"},
+    {"id": 2, "title": "Mi segundo Post con FastAPI", "content": "Mi segundo post con FastAPI blablabla"},
+    {"id": 3, "title": "Django vs FastAPI", "content": "FastAPI es más rápido por x razones"},
+    {"id": 4, "title": "Hola desde FastAPI", "content": "Mi primer post con FastAPI"},
+    {"id": 5, "title": "Mi segundo Post con FastAPI", "content": "Mi segundo post con FastAPI blablabla"},
+    {"id": 6, "title": "Django vs FastAPI", "content": "FastAPI es más rápido por x razones"},
+    {"id": 7, "title": "Hola desde FastAPI", "content": "Mi primer post con FastAPI"},
+    {"id": 8, "title": "Mi segundo Post con FastAPI", "content": "Mi segundo post con FastAPI blablabla"},
+    {"id": 9, "title": "Django vs FastAPI", "content": "FastAPI es más rápido por x razones"},
+    {"id": 10, "title": "Hola desde FastAPI", "content": "Mi primer post con FastAPI"},
+    {"id": 11, "title": "Mi segundo Post con FastAPI", "content": "Mi segundo post con FastAPI blablabla"},
+    {"id": 12, "title": "Django vs FastAPI", "content": "FastAPI es más rápido por x razones"},
+    {"id": 13, "title": "Hola desde FastAPI", "content": "Mi primer post con FastAPI"},
+    {"id": 14, "title": "Mi segundo Post con FastAPI", "content": "Mi segundo post con FastAPI blablabla"},
+    {"id": 15, "title": "Django vs FastAPI", "content": "FastAPI es más rápido por x razones"},
+    {"id": 16, "title": "Hola desde FastAPI", "content": "Mi primer post con FastAPI"},
+    {"id": 17, "title": "Mi segundo Post con FastAPI", "content": "Mi segundo post con FastAPI blablabla"},
+    {"id": 18, "title": "Django vs FastAPI", "content": "FastAPI es más rápido por x razones"},
+]
+
+for post in BLOG_POST[5:10 + 5]:
+    print(post)
